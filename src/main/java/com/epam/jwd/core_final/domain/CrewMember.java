3f760da9bd6e2c8aeac7be9e404ed18102ec1e8b
Елизавета Красновская <ElizavetaKrasnovskaya@gmail.com>
2020-12-31 18:06:09 +0300

    add domain

--- src/main/java/com/epam/jwd/core_final/domain/CrewMember.java
+++ src/main/java/com/epam/jwd/core_final/domain/CrewMember.java
@@ -8,5 +8,49 @@ package com.epam.jwd.core_final.domain;
  * isReadyForNextMissions {@link Boolean} - true by default. Set to false, after first failed mission
  */
 public class CrewMember extends AbstractBaseEntity {
-    // todo
+
+    private Role role;
+    private Rank rank;
+    private Boolean isReadyForNextMissions;
+
+    public CrewMember(String name, Role role, Rank rank) {
+        super(name);
+        this.role = role;
+        this.rank = rank;
+        isReadyForNextMissions = true;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public Rank getRank() {
+        return rank;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+
+    public void setRank(Rank rank) {
+        this.rank = rank;
+    }
+
+    public void setReadyForNextMissions(Boolean readyForNextMissions) {
+        isReadyForNextMissions = readyForNextMissions;
+    }
+
+    public Boolean getReadyForNextMissions() {
+        return isReadyForNextMissions;
+    }
+
+    @Override
+    public String toString() {
+        return "CrewMember " +
+                "name = " + getName() +
+                ", id = " + getId() +
+                ", role = " + role +
+                ", rank = " + rank +
+                ", isReadyForNextMissions = " + isReadyForNextMissions;
+    }
 }
--- src/main/java/com/epam/jwd/core_final/domain/FlightMission.java
+++ src/main/java/com/epam/jwd/core_final/domain/FlightMission.java
@@ -1,5 +1,8 @@
 package com.epam.jwd.core_final.domain;
 
+import java.time.LocalDate;
+import java.util.List;
+
 /**
  * Expected fields:
  * <p>
@@ -12,5 +15,77 @@ package com.epam.jwd.core_final.domain;
  * missionResult {@link MissionResult}
  */
 public class FlightMission extends AbstractBaseEntity {
-    // todo
+
+    private LocalDate startDate;
+    private LocalDate endDate;
+    private Long distance;
+    private Spaceship assignedSpaceShip;
+    private List<CrewMember> assignedCrew;
+    private MissionResult missionResult;
+
+    public FlightMission(String missionName, LocalDate startDate, LocalDate endDate, long distance) {
+        super(missionName);
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.distance = distance;
+    }
+
+    public LocalDate getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(LocalDate startDate) {
+        this.startDate = startDate;
+    }
+
+    public LocalDate getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(LocalDate endDate) {
+        this.endDate = endDate;
+    }
+
+    public long getDistance() {
+        return distance;
+    }
+
+    public void setDistance(long distance) {
+        this.distance = distance;
+    }
+
+    public Spaceship getAssignedSpaceShip() {
+        return assignedSpaceShip;
+    }
+
+    public void setAssignedSpaceShip(Spaceship assignedSpaceShip) {
+        this.assignedSpaceShip = assignedSpaceShip;
+    }
+
+    public List<CrewMember> getAssignedCrew() {
+        return assignedCrew;
+    }
+
+    public void setAssignedCrew(List<CrewMember> assignedCrew) {
+        this.assignedCrew = assignedCrew;
+    }
+
+    public MissionResult getMissionResult() {
+        return missionResult;
+    }
+
+    public void setMissionResult(MissionResult missionResult) {
+        this.missionResult = missionResult;
+    }
+
+    @Override
+    public String toString() {
+        return "FlightMission " +
+                "startDate = " + startDate +
+                ", endDate = " + endDate +
+                ", distance = " + distance +
+                ", assignedSpaceShip = " + assignedSpaceShip +
+                ", assignedCrew = " + assignedCrew +
+                ", missionResult = " + missionResult;
+    }
 }
