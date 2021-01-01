f87d074f8a41912fb586607fbc1453fa7d047459
Елизавета Красновская <ElizavetaKrasnovskaya@gmail.com>
2021-01-01 20:45:54 +0300

    factory

---
+++ src/main/java/com/epam/jwd/core_final/factory/impl/FlightMissionFactory.java
@@ -0,0 +1,22 @@
+package com.epam.jwd.core_final.factory.impl;
+
+import com.epam.jwd.core_final.domain.FlightMission;
+import com.epam.jwd.core_final.factory.EntityFactory;
+
+import java.time.LocalDate;
+
+public class FlightMissionFactory implements EntityFactory<FlightMission> {
+
+    private FlightMissionFactory(){}
+
+    private final static FlightMissionFactory INSTANCE = new FlightMissionFactory();
+
+    public static FlightMissionFactory getInstance(){
+        return INSTANCE;
+    }
+
+    @Override
+    public FlightMission create(Object... args) {
+        return new FlightMission((String) args[0], (LocalDate) args[1], (LocalDate) args[2], (Long) args[3]);
+    }
+}
