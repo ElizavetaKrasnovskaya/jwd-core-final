c2aa25960e3311edba17cc6556994afafce30d03
Елизавета Красновская <ElizavetaKrasnovskaya@gmail.com>
2021-01-10 12:33:08 +0300

    Main

--- src/main/java/com/epam/jwd/core_final/Main.java
+++ src/main/java/com/epam/jwd/core_final/Main.java
@@ -1,10 +1,20 @@
 package com.epam.jwd.core_final;
 
 import com.epam.jwd.core_final.context.Application;
+import com.epam.jwd.core_final.context.ApplicationMenu;
+import com.epam.jwd.core_final.exception.InvalidStateException;
+
+import java.io.IOException;
 
 public class Main {
 
     public static void main(String[] args) {
-//        Application.start();
+        ApplicationMenu applicationMenu;
+        try {
+            applicationMenu = Application.start();
+            applicationMenu.handleUserInput();
+        } catch (IOException | InvalidStateException e) {
+            System.out.println(e);
+        }
     }
 }
