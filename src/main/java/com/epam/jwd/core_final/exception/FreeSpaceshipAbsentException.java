4c25ff554d8ee0b4704bf6c2c44f8107763b8e9a
Елизавета Красновская <ElizavetaKrasnovskaya@gmail.com>
2021-01-10 12:31:53 +0300

    exception

---
+++ src/main/java/com/epam/jwd/core_final/exception/FreeSpaceshipAbsentException.java
@@ -0,0 +1,9 @@
+package com.epam.jwd.core_final.exception;
+
+public class FreeSpaceshipAbsentException extends RuntimeException {
+
+    public FreeSpaceshipAbsentException(String message) {
+        super(message);
+    }
+
+}
